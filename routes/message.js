8efa2404df1ecb5530909b7fd8f2bf87ee7469df
simantar <simanta@hashtag-ca.com>
2023-08-09 11:11:38 +0530

    message pagination

--- routes/message.js
+++ routes/message.js
@@ -2,21 +2,26 @@ const router = require("express").Router();
 const connection = require("../mysql");
 
 // get all  messages
-router.get("/all/sender=:senderId/receiver=:receiverId", (req, res) => {
+router.get("/all/sender=:senderId/receiver=:receiverId/:page?", (req, res) => {
   const senderId = req.params.senderId;
   const receiverId = req.params.receiverId;
+  const page = req.params.page ? parseInt(req.params.page) : 1; // Default to page 1 if not specified
+  const itemsPerPage = 10; // Number of messages per page
+
+  const offset = (page - 1) * itemsPerPage;
 
   const sql = `
-      SELECT *
-      FROM messages
-      WHERE (sender_id = ? AND receiver_id = ?)
-         OR (sender_id = ? AND receiver_id = ?)
-      ORDER BY timestamp;
-    `;
+    SELECT *
+    FROM messages
+    WHERE (sender_id = ? AND receiver_id = ?)
+       OR (sender_id = ? AND receiver_id = ?)
+    ORDER BY timestamp
+    LIMIT ? OFFSET ?;
+  `;
 
   connection.query(
     sql,
-    [senderId, receiverId, receiverId, senderId],
+    [senderId, receiverId, receiverId, senderId, itemsPerPage, offset],
     (error, results) => {
       if (error) {
         console.error("Error fetching messages:", error);
