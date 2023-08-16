c58a711c2b6accf078f3aab346b64fe2090212c7
simantar <simanta@hashtag-ca.com>
2023-08-16 16:07:33 +0530

    delete otp data

--- routes/auth.js
+++ routes/auth.js
@@ -4,62 +4,81 @@ const JWT = require("jsonwebtoken");
 const bcrypt = require("bcrypt");
 const { clientTwilio } = require("../utils/twilio");
 
-const otpStore = {};
-
 // new user otp
 router.post("/register", async (req, res) => {
   const { userName, email, password, phoneNumber } = req.body;
 
   const otp = Math.floor(100000 + Math.random() * 900000);
-  otpStore[userName] = otp;
   const otpMessage = `Your OTP is: ${otp}. Please enter this code to verify your account.`;
+
+  const insertQuery =
+    "INSERT INTO temporary_otp (username, phone_number,otp) VALUES (?,?,?)";
 
   clientTwilio.messages
     .create({
       from: process.env.TWILIO_PHONO,
-      to: phoneNumber,
+      // to: phoneNumber,
+      to: "+917002672204",
       body: otpMessage,
     })
-    .then((message) => console.log("opt send"));
-  res.status(201).json({ userName, email, password, phoneNumber });
+    .then((message) => {
+      connection.query(
+        insertQuery,
+        [userName, phoneNumber, otp],
+        (error, results) => {
+          if (error) {
+            console.log(error);
+            res.status(500).json({ error: "Error user data" });
+          } else {
+            res.status(201).json({ userName, email, password, phoneNumber });
+          }
+        }
+      );
+    });
 });
 
 // verifying otp will create a new user
 router.post("/verifyotp", (req, res) => {
   const { userName, email, password, phoneNumber, enteredOtp } = req.body;
 
-  const storedOtp = otpStore[userName];
   const passwordHash = bcrypt.hashSync(password, 10);
+
+  const otpQuery =
+    "SELECT otp FROM temporary_otp WHERE userName=? AND expires_at > NOW() ORDER BY created_at desc limit 1";
 
   const insertQuery =
     "INSERT INTO users (username, email,phone_no,password) VALUES (?,?,?,?)";
 
-  if (storedOtp && enteredOtp === storedOtp.toString()) {
-    delete otpStore[userName];
-
-    connection.query(
-      insertQuery,
-      [userName, email, phoneNumber, passwordHash],
-      (error, results) => {
-        if (error) {
-          res.status(500).json({ error: "Error user data" });
-        } else {
-          const user = results;
-          const accessToken = JWT.sign(
-            {
-              id: user.insertId,
-              userName,
-            },
-            process.env.PASSTOKEN,
-            { expiresIn: "5h" }
-          );
-          res.status(200).json({ userName, accessToken });
-        }
+  connection.query(otpQuery, [userName], (error, results) => {
+    if (error) {
+      res.status(500).json({ error: "Error user data" });
+    } else {
+      if (results[0].otp && enteredOtp === results[0].otp.toString()) {
+        connection.query(
+          insertQuery,
+          [userName, email, phoneNumber, passwordHash],
+          (error, results) => {
+            if (error) {
+              res.status(500).json({ error: "Error user data" });
+            } else {
+              const user = results;
+              const accessToken = JWT.sign(
+                {
+                  id: user.insertId,
+                  userName,
+                },
+                process.env.PASSTOKEN,
+                { expiresIn: "5h" }
+              );
+              res.status(200).json({ userName, accessToken });
+            }
+          }
+        );
+      } else {
+        res.status(401).json({ error: "Invalid OTP" });
       }
-    );
-  } else {
-    res.status(401).json({ error: "Invalid OTP" });
-  }
+    }
+  });
 });
 
 // login
